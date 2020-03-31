f38e51254e1f226244da4d790bb8a6289ad9fc28
Cory iMac27 MD17 <cory@Cory-iMac27-MD17.local>
2020-03-31 16:42:31 -0700

    WU to Home

--- src/Nav.js
+++ src/Nav.js
@@ -4,7 +4,11 @@ import{Link}from 'react-router-dom'
 export default function Nav(){
     return<div>
         <nav className='nav'>
-<div>WestSide University</div> 
+<div>
+    <Link to="/">
+    WestSide University
+    </Link>
+</div> 
 <div className='link-wrap'>
     <Link to="/" className='links'>Home</Link>
     <Link to="/about" className='links'>About</Link> 
